d72f4d81e9f15af4efc358f92085e00ca0662e3d
ehosaini <me.hosaini@gmail.com>
2018-04-07 10:28:43 -0400

    Setup initial project

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Express!');
+})
+
+app.listen(3000, () => {
+  console.log('Sever up at 3000')
+});

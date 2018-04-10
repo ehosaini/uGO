ad004f3bf6f7198253d6ebe20806813b9f079c25
ehosaini <me.hosaini@gmail.com>
2018-04-10 19:56:17 -0400

    Add JSON API endpoint for countries list

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const {
+  Country
+} = require('./models/country');
 
 app = express();
 
+// GET Countries API enpoint
 app.get('/', (req, res) => {
-  res.send('Hello Express!');
-})
+  let projection = `Name Alpha2Code Alpha3Code Latitude Longitude NumericCode \
+  NativeLanguage CurrencyCode CurrencyName CurrencySymbol`;
+
+  Country.find(null, projection).then((countries) => {
+    res.send(countries);
+  });
+
+});
 
 app.listen(3000, () => {
   console.log('Sever up at 3000');

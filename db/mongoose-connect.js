d7c69d1b31dd585cf50999f22ad696c506add24c
ehosaini <me.hosaini@gmail.com>
2018-05-13 12:46:24 -0400

    Fix db connection Promise to catch errors

--- db/mongoose-connect.js
+++ db/mongoose-connect.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/uGoApp');
+mongoose.connect('mongodb://localhost:27017/uGoApp').then((connection) => {
+
+}).catch((e) => {
+  console.log(e);
+});
 
 module.exports = {
   mongoose
